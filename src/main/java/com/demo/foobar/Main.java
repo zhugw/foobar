26b387acfc042666a0b09b4133b6e4bbd205e59b
朱国伟 <zhugw@zhuguoweideMacBook-Pro.local>
2016-12-28 21:21:11 +0800

    first commit

---
+++ src/main/java/com/demo/foobar/Main.java
@@ -0,0 +1,14 @@
+package com.demo.foobar;
+
+import com.demo.bar.SkinEntity;
+import com.demo.foo.SkinServiceImpl;
+
+/**
+ * Created by duanmu on 12/28/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        SkinServiceImpl app = new SkinServiceImpl();
+        app.entityToSkin(new SkinEntity());
+    }
+}
